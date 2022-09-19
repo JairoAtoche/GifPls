32e8b4b970967f7239d0c1d67c81b5f25a32da50
JairoAtoche <jairoatoche90098@gmail.com>
2022-09-18 20:43:35 -0500

    add: configuración de alias

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import { fileURLToPath, URL } from "url";
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@/assets': fileURLToPath(new URL('./src/assets/', import.meta.url)),
+      '@/components': fileURLToPath(new URL('./src/components/', import.meta.url)),
+      '@/pages': fileURLToPath(new URL('./src/pages/', import.meta.url)),
+      '@/styles': fileURLToPath(new URL('./src/styles/', import.meta.url))
+    }
+  },
   plugins: [react()]
 })
