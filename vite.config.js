5469fad69ac36973adef675db24ae66960296549
JairoAtoche <jairoatoche90098@gmail.com>
2022-09-20 21:30:04 -0500

    add: configuracion prettier, eslint, package, vite

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { fileURLToPath, URL } from "url";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { fileURLToPath, URL } from 'url';
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      '@/assets': fileURLToPath(new URL('./src/assets/', import.meta.url)),
-      '@/components': fileURLToPath(new URL('./src/components/', import.meta.url)),
-      '@/pages': fileURLToPath(new URL('./src/pages/', import.meta.url)),
-      '@/styles': fileURLToPath(new URL('./src/styles/', import.meta.url))
-    }
-  },
-  plugins: [react()]
-})
+	resolve: {
+		alias: {
+			'@/public': fileURLToPath(new URL('./public/', import.meta.url)),
+			'@/assets': fileURLToPath(new URL('./src/assets/', import.meta.url)),
+			'@/layouts': fileURLToPath(new URL('./src/layouts/', import.meta.url)),
+			'@/components': fileURLToPath(
+				new URL('./src/components/', import.meta.url)
+			),
+			'@/pages': fileURLToPath(new URL('./src/pages/', import.meta.url)),
+			'@/styles': fileURLToPath(new URL('./src/styles/', import.meta.url)),
+		},
+	},
+	plugins: [react()],
+});
