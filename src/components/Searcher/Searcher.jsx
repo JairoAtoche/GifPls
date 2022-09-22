e94890d0caa5d648972a73686b0c634a50a6cb82
JairoAtoche <jairoatoche90098@gmail.com>
2022-09-22 12:35:27 -0500

    add: estilos para el componente searcher

---
+++ src/components/Searcher/Searcher.jsx
@@ -0,0 +1,19 @@
+import React, { useContext } from 'react';
+import './searcher.scss';
+import { ThemeContext } from '@/context/ThemeContext';
+import '@/styles/theme.scss';
+
+const Searcher = () => {
+	const { theme } = useContext(ThemeContext);
+	return (
+		<div className='diva'>
+			<input
+				className={`searcher-input theme-input--${theme}`}
+				placeholder='Escribe una palabra'
+			/>
+			<button className='searcher-btn'>BUSCAR</button>
+		</div>
+	);
+};
+
+export default Searcher;
--- src/pages/Gifs/Gifs.jsx
+++ src/pages/Gifs/Gifs.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
-
+import React, { useContext } from 'react';
+import './gifs.scss';
+import '@/styles/theme.scss';
+import { ThemeContext } from '@/context/ThemeContext';
+import Searcher from '@/components/Searcher/Searcher';
 const Gifs = () => {
+	const { theme } = useContext(ThemeContext);
 	return (
-		<main>
-			<h1>Página GIFS</h1>
+		<main className={`gif theme-page--${theme}`}>
+			<section className='section-intro'>
+				<h1 className='section-intro__h1'>GIFPls</h1>
+				<p className='section-intro__p'>
+					Encuentra los mejores gif, stickers animados y clips con sonido para
+					darle diversión y transmitir visualmente tus mensajes.
+				</p>
+				<Searcher />
+			</section>
+			<section className='section-gif'>
+				<h2>Gif</h2>
+			</section>
+			<section className='section-trend'>
+				<h2>Últimas tendencias</h2>
+			</section>
 		</main>
 	);
 };
