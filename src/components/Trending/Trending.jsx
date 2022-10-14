836e75f6f605f60a214e229481cdb15c749922da
JairoAtoche <jairoatoche90098@gmail.com>
2022-10-13 21:06:40 -0500

    update: agregado buscador al seleccionar la tendencia

--- src/components/Trending/Trending.jsx
+++ src/components/Trending/Trending.jsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './trending.scss';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import { SearcherContext } from '@/context/SearcherContext';
 
 const Trending = ({ name }) => {
 	const id = name.split(' ').join('+');
+	const { setSearcher } = useContext(SearcherContext);
+
+	const handleTrendring = () => {
+		window.scrollTo(0, 0);
+		setSearcher(id);
+	};
+
 	return (
-		<h3 className='trending'>
-			<Link to={id}>{name}</Link>
+		<h3 className='trending' onClick={handleTrendring}>
+			<Link to={`?search=${id}`}>{name}</Link>
 		</h3>
 	);
 };
