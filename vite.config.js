1b50235f682dba745cfa8678be61fb4aaf40f151
JairoAtoche <jairoatoche90098@gmail.com>
2022-10-22 23:01:22 -0500

    update: refactorizado alias del vite config y agregado jsconfig para conectar las rutas absolutas con el jsdoc

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { fileURLToPath, URL } from 'url';
-// https://vitejs.dev/config/
+import path from 'node:path';
+
 export default defineConfig({
 	resolve: {
 		alias: {
-			'@/public': fileURLToPath(new URL('./public/', import.meta.url)),
-			'@/assets': fileURLToPath(new URL('./src/assets/', import.meta.url)),
-			'@/layouts': fileURLToPath(new URL('./src/layouts/', import.meta.url)),
-			'@/components': fileURLToPath(
-				new URL('./src/components/', import.meta.url)
-			),
-			'@/pages': fileURLToPath(new URL('./src/pages/', import.meta.url)),
-			'@/styles': fileURLToPath(new URL('./src/styles/', import.meta.url)),
-			'@/router': fileURLToPath(
-				new URL('./src/config/router/', import.meta.url)
-			),
-			'@/context': fileURLToPath(new URL('./src/context/', import.meta.url)),
-			'@/services': fileURLToPath(new URL('./src/services/', import.meta.url)),
-			'@/hooks': fileURLToPath(new URL('./src/hooks/', import.meta.url)),
+			'@/public': path.resolve(__dirname, './public/'),
+			'@/assets': path.resolve(__dirname, './src/assets/'),
+			'@/components': path.resolve(__dirname, './src/components/'),
+			'@/router': path.resolve(__dirname, './src/config/router/'),
+			'@/context': path.resolve(__dirname, './src/context/'),
+			'@/hooks': path.resolve(__dirname, './src/hooks/'),
+			'@/layouts': path.resolve(__dirname, './src/layouts/'),
+			'@/pages': path.resolve(__dirname, './src/pages/'),
+			'@/services': path.resolve(__dirname, './src/services/'),
+			'@/styles': path.resolve(__dirname, './src/styles/'),
 		},
 	},
 	plugins: [react()],
