3bd7b4d56eafb027c3b3e41a6ff630dd2f26a46f
JairoAtoche <jairoatoche90098@gmail.com>
2022-11-09 18:35:45 -0500

    update: cambiado el enrutado de browserRouter a hashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import '@/styles/normalize.scss';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { ThemeProvider } from '@/context/ThemeContext';
 import { SearcherProvider } from '@/context/SearcherContext';
 ReactDOM.createRoot(document.getElementById('root')).render(
-	<BrowserRouter>
+	<HashRouter>
 		<ThemeProvider>
 			<SearcherProvider>
 				<App />
 			</SearcherProvider>
 		</ThemeProvider>
-	</BrowserRouter>
+	</HashRouter>
 );
